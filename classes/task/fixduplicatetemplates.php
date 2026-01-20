iomad/iomad

IOMAD: need to re-run any add template tasks in case they didn't run due to issues with the task. Also - remove any/all duplicate email templates for companies which were caused during migration. #2547

local/email/classes/task/fixduplicatetemplates.php
@@ -1,0 +1,102 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * An adhoc task for local IOMAD emails
+ *
+ * @package    local_iomad
+ * @copyright  2025 E-Learn Design https://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace local_email\task;
+
+use core\task\adhoc_task;
+use local_email;
+use core\task\manager;
+
+/**
+ * An adhoc task for local IOMAD emails
+ *
+ * @package    local_iomad
+ * @copyright  2025 E-Learn Design https://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class fixduplicatetemplates extends adhoc_task {
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('fixduplicatetemplatesadhoc', 'local_email');
+    }
+
+    /**
+     * Run fixduplicatetemplates
+     */
+    public function execute() {
+        global $DB;
+
+        // Get all of the companies.
+        $companies = $DB->get_records('company', [], '', 'id');
+
+        // And all the template names.
+        $templates = $DB->get_records_sql("SELECT DISTINCT name FROM {email_template}");
+
+        // Define the bit of SQL as it won't change.
+        $deletesql = "companyid = :companyid
+                      AND name = :name
+                      AND id != :id";
+
+        // Process them.
+        foreach ($companies as $company) {
+            foreach ($templates as $template) {
+                // Get the first instance of this companyid and template name
+                // as we want to keep just that one.
+                $firstrec = $DB->get_records('email_template',
+                                             ['companyid' => $company->id,
+                                              'name' => $template->name],
+                                             'id',
+                                             'id',
+                                             0,
+                                             1);
+                $first = array_pop($firstrec);
+
+                // Delete everything else.
+                $DB->delete_records_select('email_template',
+                                           $deletesql,
+                                           ['companyid' => $company->id,
+                                            'name' => $template->name,
+                                            'id' => $first->id]);
+
+            }
+        }
+    }
+
+    /**
+     * Queues the task.
+     *
+     */
+    public static function queue_task() {
+
+        // Let's set up the adhoc task.
+        $task = new fixduplicatetemplates();
+        manager::queue_adhoc_task($task, true);
+    }
+}

local/email/db/upgrade.php
@@ -981,6 +981,39 @@ function xmldb_local_email_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2025012002, 'local', 'email');
     }
 
-    return $result;
+    if ($oldversion < 2025123000) {
 
+        // Need to re-run these tasks due to issues with these tasks not working with new
+        // database structure.
+        $templates = [
+            'user_signed_up_to_waitlist',
+            'user_signed_up_for_event_reminder',
+            'expiring_digest_manager',
+            'warning_digest_manager',
+        ];
+
+        // Set up an ad-hoc task to re-add the new email templates - so we ensure we have them.
+        foreach ($templates as $template) {
+            $addtask = new local_email\task\addtemplate();
+            $addtask->set_custom_data([
+                'templatename' => $template,
+                'disabled' => 1,
+            ]);
+
+            // Queue the task.
+            core\task\manager::queue_adhoc_task($addtask);
+        }
+
+        // We may also have ended up with duplicates in the email_template table so
+        // run the ad-hoc task for that.
+        $addtask = new local_email\task\fixduplicatetemplates();
+
+        // Queue the task.
+        core\task\manager::queue_adhoc_task($addtask);
+
+        // Email savepoint reached.
+        upgrade_plugin_savepoint(true, 2025123000, 'local', 'email');
+    }
+
+    return $result;
 }

iomad/moodle-local_iomad

classes/task/fixduplicatetemplates.php
@@ -1,0 +1,101 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * An adhoc task for local IOMAD emails
+ *
+ * @package    local_iomad
+ * @copyright  2025 E-Learn Design https://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace local_iomad\task;
+
+use core\task\adhoc_task;
+use core\task\manager;
+
+/**
+ * An adhoc task for local IOMAD emails
+ *
+ * @package    local_iomad
+ * @copyright  2025 E-Learn Design https://www.e-learndesign.co.uk
+ * @author     Derick Turner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class fixduplicatetemplates extends adhoc_task {
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('fixduplicatetemplatesadhoc', 'local_iomad');
+    }
+
+    /**
+     * Run fixduplicatetemplates
+     */
+    public function execute() {
+        global $DB;
+
+        // Get all of the companies.
+        $companies = $DB->get_records('company', [], '', 'id');
+
+        // And all the template names.
+        $templates = $DB->get_records_sql("SELECT DISTINCT name FROM {email_template}");
+
+        // Define the bit of SQL as it won't change.
+        $deletesql = "companyid = :companyid
+                      AND name = :name
+                      AND id != :id";
+
+        // Process them.
+        foreach ($companies as $company) {
+            foreach ($templates as $template) {
+                // Get the first instance of this companyid and template name
+                // as we want to keep just that one.
+                $firstrec = $DB->get_records('email_template',
+                                             ['companyid' => $company->id,
+                                              'name' => $template->name],
+                                             'id',
+                                             'id',
+                                             0,
+                                             1);
+                $first = array_pop($firstrec);
+
+                // Delete everything else.
+                $DB->delete_records_select('email_template',
+                                           $deletesql,
+                                           ['companyid' => $company->id,
+                                            'name' => $template->name,
+                                            'id' => $first->id]);
+
+            }
+        }
+    }
+
+    /**
+     * Queues the task.
+     *
+     */
+    public static function queue_task() {
+
+        // Let's set up the adhoc task.
+        $task = new fixduplicatetemplates();
+        manager::queue_adhoc_task($task, true);
+    }
+}

db/upgrade.php
@@ -2601,6 +2601,40 @@ function xmldb_local_iomad_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2025070200, 'local', 'iomad');
     }
 
+    if ($oldversion < 2025123000) {
+
+        // Need to re-run these tasks due to issues with these tasks not working with new
+        // database structure. 
+        $templates = [
+            'user_signed_up_to_waitlist', 
+            'user_signed_up_for_event_reminder',
+            'expiring_digest_manager',
+            'warning_digest_manager', 
+        ];
+ 
+        // Set up an ad-hoc task to re-add the new email templates - so we ensure we have them.
+        foreach ($templates as $template) {
+            $addtask = new local_iomad\task\addtemplate();
+            $addtask->set_custom_data([
+                'templatename' => $template,
+                'disabled' => 1,
+            ]);
+
+            // Queue the task.
+            core\task\manager::queue_adhoc_task($addtask);
+        }
+
+        // We may also have ended up with duplicates in the email_template table so
+        // run the ad-hoc task for that.
+        $addtask = new local_iomad\task\fixduplicatetemplates();
+
+        // Queue the task.
+        core\task\manager::queue_adhoc_task($addtask);
+
+        // Email savepoint reached.
+        upgrade_plugin_savepoint(true, 2025123000, 'local', 'email');
+    }
+
     if ($oldversion < 2025123100) {
         // Moving IOMAD settings from local_iomad_settings plugin using $CFG to local_iomad get_config.
         mtrace("");